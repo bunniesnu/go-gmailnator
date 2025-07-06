64daab05fdb100a97c1d68f6fd6fc9413507543f
Bunniesnu <bunniesnu@gmail.com>
2025-07-06 13:27:42 +0900

    add: GetMailBody testing w/ sending test email through gmail smtp

--- gmailnator_test.go
+++ gmailnator_test.go
@@ -1,10 +1,45 @@
 package gmailnator
 
 import (
+	"fmt"
+	"net/smtp"
+	"os"
+	"strings"
 	"testing"
+	"time"
 )
 
+func SendTestMail(subject, body, destination string) error {
+	from := os.Getenv("SMTP_FROM")
+	password := os.Getenv("SMTP_PASSWORD")
+	to := []string{destination}
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	headers := []string{
+		"From: " + from,
+		"To: " + strings.Join(to, ","),
+		fmt.Sprintf("Subject: %s", subject),
+		body,
+	}
+	message := []byte(strings.Join(headers, "\r\n"))
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		return fmt.Errorf("failed to send email: %w", err)
+	}
+	return nil
+}
+
 func TestNewGmailnator(t *testing.T) {
+	// Check if environment variables are set
+	if os.Getenv("SMTP_FROM") == "" || os.Getenv("SMTP_PASSWORD") == "" {
+		t.Skip("Skipping test because SMTP_FROM or SMTP_PASSWORD is not set in environment variables")
+	}
+
 	// Test NewGmailnator
 	gmailnator, err := NewGmailnator()
 	if err != nil {
@@ -16,6 +51,7 @@ func TestNewGmailnator(t *testing.T) {
 	if gmailnator.Client == nil {
 		t.Error("Expected Client to be initialized, got nil")
 	}
+	t.Log("Gmailnator instance create success")
 
 	// Test GenerateEmail
 	err = gmailnator.GenerateEmail()
@@ -40,13 +76,50 @@ func TestNewGmailnator(t *testing.T) {
 	if gmailnator.RapidAPI == "" {
 		t.Error("Expected RapidAPI to be set, got empty string")
 	}
+	t.Logf("Generated email: %s at %d", gmailnator.Email.Email, gmailnator.Email.Timestamp)
+	
+	// Send a test email
+	destination := gmailnator.Email.Email
+	subject := "Greetings from Go\r\n"
+	body := "This is a test email from a Go program.\r\n"
+	err = SendTestMail(subject, body, destination)
+	if err != nil {
+		t.Errorf("Failed to send test email: %v", err)
+	}
+	trimmedExpectedSubject := strings.TrimSpace(subject)
+	trimmedExpectedBody := strings.TrimSpace(body)
+	t.Logf("Test email sent to %s with subject '%s'", destination, trimmedExpectedSubject)
 
 	// Test GetMails
-	email, err := gmailnator.GetMails()
-	if err != nil {
-		t.Errorf("Failed to get mails: %v", err)
+	for range 5 {
+		email, err := gmailnator.GetMails()
+		if err != nil {
+			t.Errorf("Failed to get mails: %v", err)
+		}
+		if email == nil {
+			t.Error("Expected email to be returned, got nil")
+		}
+		for _, mail := range email {
+			messageId := mail.Mid
+			mailDetails, err := gmailnator.GetMailBody(messageId)
+			if err != nil {
+				t.Errorf("Failed to get mail details for message ID %s: %v", messageId, err)
+			}
+			if mailDetails == "" {
+				t.Errorf("Expected mail details for message ID %s, got empty", messageId)
+			}
+			trimmedMailSubject := strings.TrimSpace(mail.Subject)
+			trimmedMailDetails := strings.TrimSpace(mailDetails)
+			if trimmedMailSubject != trimmedExpectedSubject {
+				t.Errorf("Expected mail subject '%s', got '%s'", trimmedExpectedSubject, trimmedMailSubject)
+			}
+			if trimmedMailDetails != trimmedExpectedBody {
+				t.Errorf("Expected mail body '%s', got '%s'", trimmedExpectedBody, trimmedMailDetails)
+			}
+			t.Logf("Received email from %s with subject '%s' and body '%s'", mail.From, trimmedMailSubject, trimmedMailDetails)
+			return
+		}
+		time.Sleep(5 * time.Second) // Wait for 5 seconds before the next iteration
 	}
-	if email == nil {
-		t.Error("Expected email to be returned, got nil")
-	}
+	t.Errorf("Failed to retrieve any emails after multiple attempts")
 }
